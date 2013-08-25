1bc42ad94fc8f5584082204954e4a066c4ee6fce
Im-Laodicean <im.laodicean@gmail.com>
2013-08-25 14:27:07 -0400

    Nothing.

--- TSG/src/engine/GameEngine.java
+++ TSG/src/engine/GameEngine.java
@@ -37,8 +37,8 @@ public class GameEngine {
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
+		
 		GL11.glViewport(0,0,800,600);
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
