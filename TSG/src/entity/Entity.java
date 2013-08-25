fd3adb3cef97a1bd9c05b9ebfee3700abdff7029
JJLuan <JohnLuan96@gmail.com>
2013-08-25 14:14:36 -0400

    Fleshed out Entity, added PlayerEntity

--- TSG/src/entity/Entity.java
+++ TSG/src/entity/Entity.java
@@ -4,4 +4,35 @@ public abstract class Entity {
 
 	protected int x,y,width,height;
 	
+	public Entity(){
+		x = 0;
+		y = 0;
+		width = 0;
+		height = 0;
+	}
+	
+	public Entity(int x, int y, int width, int height){
+		
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+	
+	protected abstract void setX();
+	protected abstract void setY();
+	protected abstract void setWidth();
+	protected abstract void setHeight();
 }
---
+++ TSG/src/entity/PlayerEntity.java
@@ -0,0 +1,30 @@
+package entity;
+
+public class PlayerEntity extends Entity {
+
+	
+	@Override
+	protected void setX() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setY() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setWidth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setHeight() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
