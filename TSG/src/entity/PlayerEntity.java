c1c35513cbd6546dc621a435cb09c98ebbe757be
JJLuan <JohnLuan96@gmail.com>
2013-08-26 21:13:10 -0400

    Revert "Merged"
    This reverts commit cc41a1ea975bd5f4e720e06cd7b1185da291f6e3, reversing
    changes made to ff69b8071cd2acb0ed1cdc6663ebaaff161e13c5.
    
    	modified:   TSG/bin/.gitignore
    	deleted:    TSG/bin/engine/GameEngine.class
    	deleted:    TSG/src/engine/GameEngine.java
    	modified:   TSG/src/entity/PlayerEntity.java
    	modified:   TSG/src/render/Sprite.java

--- TSG/src/entity/PlayerEntity.java
+++ TSG/src/entity/PlayerEntity.java
@@ -12,16 +12,14 @@ public class PlayerEntity extends Entity implements RenderableObject{
 	private String name;
 	private int key;
 	private Sprite player;
-	
 	public PlayerEntity(String name) throws IOException{
 		super(20,20,100,100);
-		this.setName(name);
+		this.name = name;
 		key = name.hashCode();
-		ResourceManager.loadTexture("Riven.png", key);
-		player = new Sprite(key, this.getWidth(), this.getHeight(), 4, 4);
+		ResourceManager.loadTexture("test.png", key);
+		player = new Sprite(key, 100, 100, 2, 2);
 	}
 	
-<<<<<<< HEAD
 	@Override
 	public void setX() {
 		// TODO Auto-generated method stub
@@ -43,41 +41,18 @@ public class PlayerEntity extends Entity implements RenderableObject{
 	@Override
 	public void setHeight() {
 		// TODO Auto-generated method stub
-=======
-	protected void setX() {
 		
 	}
 
-	protected void setY() {
-		
-	}
-
-	protected void setWidth() {
-	
-	}
-
-	protected void setHeight() {
->>>>>>> origin/god
-		
-	}
-	
+	@Override
 	public void renderSelf() {
 		player.drawNextFrame(x, y);
 	}
 
-<<<<<<< HEAD
 	@Override
 	public boolean isRenderable() {
 		// TODO Auto-generated method stub
 		return false;
-=======
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
->>>>>>> origin/god
 	}
 	
 }
