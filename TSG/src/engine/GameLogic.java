1ca0f4c920e914aab31132f19a160ae417864f6e
JJLuan <JohnLuan96@gmail.com>
2013-08-26 22:43:35 -0400

    Comments on the core classes
    Also changed Display.sync from 20 to 60.

--- TSG/src/engine/GameLogic.java
+++ TSG/src/engine/GameLogic.java
@@ -7,13 +7,30 @@ import org.lwjgl.input.Keyboard;
 
 import entity.Entity;
 
+/**Handles all interactions with <code>Entity</code>s 
+ * 
+ * @author John
+ *
+ */
 public class GameLogic {
+	
+	/**List of all entities
+	 * 
+	 */
 	public static List<Entity> entities;
 	
+	/**Initializes Entity list
+	 * 
+	 */
 	public static void init(){
 		entities = new ArrayList<Entity>();
 	}
 	
+	/**Adds an Entity. If the Entity returns <code>true</code> on <code>isRenderable</code>,
+	 * it is also added GameRenderManager through <code>addRenderable(e)</code>
+	 * 
+	 * @param e
+	 */
 	public static void addEntity(Entity e){
 		entities.add(e);
 		if(e.isRenderable())
--- TSG/src/engine/GameLoop.java
+++ TSG/src/engine/GameLoop.java
@@ -6,7 +6,16 @@ import org.lwjgl.opengl.DisplayMode;
 
 
 public class GameLoop {
+	/**Determines if the game is to quit. 
+	 * 
+	 */
 	public static boolean isFinished;
+	
+	/**Creates the Display, sets up the appropriate OpenGL parameters,
+	 * and initializes the render manager and input handler, as well as the logic engine.
+	 * 
+	 * @throws LWJGLException
+	 */
 	public static void init() throws LWJGLException{
 		isFinished = false;
 		
@@ -28,7 +37,9 @@ public class GameLoop {
 		Display.create();
 	}
 	
-	
+	/**Runs the game using a 60 FPS cap and variable time-steps
+	 * 
+	 */
 	public static void run(){
 		long lastLoopTime = System.nanoTime();//first loop time
 		final int NOMINAL_FPS = 60;
@@ -45,7 +56,7 @@ public class GameLoop {
 			
 			timeSinceFPSCheck+=timeSinceUpdate;
 			fps++;
-			
+			//mostly debug purposes
 			if(timeSinceFPSCheck>=1000000000){
 				System.out.println(fps);
 				fps = 0;
@@ -55,11 +66,17 @@ public class GameLoop {
 			if(Display.isCloseRequested()){
 				isFinished = true;
 			}
+			//update game state with appropriate delta 
+			//and the keys it needs to respond to
 			GameLogic.update(delta, GameInputHandler.keysHeld);
+			//render
 			GameRenderManager.render();
+			//poll the keyboard
 			GameInputHandler.pollKeyboardInput();
+			
+			//update the display
 			Display.update();
-			Display.sync(20);
+			Display.sync(60);
 		}
 	}
 }
